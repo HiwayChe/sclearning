ef5883dee4c057e9f79d3ef9ae3f12c412871611
HiwayChe <hiwayche@gmail.com>
2020-06-23 23:19:43 +0800

    consumer

--- provider/src/main/java/com/hiway/provider/ProviderApplication.java
+++ provider/src/main/java/com/hiway/provider/ProviderApplication.java
@@ -2,6 +2,7 @@ package com.hiway.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
